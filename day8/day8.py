7c0fbb56be959419c6aad6450ece186584afe5dd
Bara <bara@develer.com>
2021-12-20 18:05:04 +0100

    complete day8

--- day8/day8.py
+++ day8/day8.py
@@ -1,6 +1,8 @@
 from itertools import chain
 from collections import Counter
 
+from typing import List, Set, Tuple
+
 LEN_OF_0 = 6
 LEN_OF_1 = 2
 LEN_OF_2 = 5
@@ -12,8 +14,92 @@ LEN_OF_7 = 3
 LEN_OF_8 = 7
 LEN_OF_9 = 6
 
-list_of_bits = [set('cagedb'), set('ab'), set('gcdfa'), set('fbcad'), set('eafb'), set('cdfbe'), set('cdfgeb'),
-                set('dab'), set('acedgfb'), set('cefabd')]
+
+def calculateDigit(result: List[str]) -> List[Set[str]]:
+
+    def searchForLength(length):
+        for i in result:
+            if len(i) == length:
+                return set(i)
+
+    def searchFor1():
+        return searchForLength(LEN_OF_1)
+
+    def searchFor7():
+        return searchForLength(LEN_OF_7)
+
+    def searchFor4():
+        return searchForLength(4)
+
+    def searchFor8():
+        return searchForLength(8)
+
+    def searchFor0And9(digit_0_6: Set[str], digit_for_1: Set[str]) -> Tuple[Set[str], Set[str], Set[str]]:
+        digit_for_0 = set()
+        digit_for_6 = set()
+        digit_for_9 = set()
+        for i in result:
+            if len(i) == LEN_OF_0:
+                digits = set(i)
+                if len(digit_0_6 - digits) == 1:
+                    digit_for_0 = digits
+                else:
+                    if len(digit_for_1 - digits) == 0:
+                        digit_for_9 = digits
+                    else:
+                        assert len(digit_for_1 - digits) == 1
+                        digit_for_6 = digits
+
+            if digit_for_0 and digit_for_9 and digit_for_6:
+                break
+
+        return digit_for_0, digit_for_6, digit_for_9
+
+    def searchFor3(digit_for_1: Set[str]) -> Set[str]:
+        for i in result:
+            if len(i) == LEN_OF_3 and len(digit_for_1 - set(i)) == 0:
+                return set(i)
+        return set()
+
+    def searchFor2and5(digit_5: str, digit_for_1: Set[str]) -> Tuple[Set[str], Set[str]]:
+        digit_for_2 = set()
+        digit_for_5 = set()
+        for i in result:
+            digits = set(i)
+            if len(i) == LEN_OF_2 and len(digit_for_1 - digits) == 1:
+                if digit_5 in digits:
+                    digit_for_2 = digits
+                else:
+                    digit_for_5 = digits
+
+        return digit_for_2, digit_for_5
+
+    assert len(result) == 14
+
+    digit_for_7 = searchFor7()
+    digit_for_1 = searchFor1()
+    digit_for_4 = searchFor4()
+    digit_for_8 = set('abcdefg')
+
+    upper_digit = digit_for_7 - digit_for_1
+    digit_0_6 = digit_for_4 - digit_for_1
+
+    digit_for_0, digit_for_6, digit_for_9 = searchFor0And9(digit_0_6, digit_for_1)
+    digit_for_3 = searchFor3(digit_for_1)
+
+    if digit_for_9:
+        digit_5 = digit_for_8 - digit_for_9
+    else:
+        assert digit_for_3
+        digit_5 = digit_for_8 - digit_for_4 - digit_for_3
+
+    assert len(digit_5) == 1
+    digit_for_2, digit_for_5 = searchFor2and5(next(iter(digit_5)), digit_for_1)
+
+    assert upper_digit and digit_0_6
+
+    return [digit_for_0, digit_for_1, digit_for_2, digit_for_3, digit_for_4, digit_for_5, digit_for_6,
+            digit_for_7, digit_for_8, digit_for_9]
 
 
 def main():
@@ -25,10 +111,17 @@ def main():
     c = Counter(map(len, chain(*results)))
     print("Part 1:", c[LEN_OF_1] + c[LEN_OF_4] + c[LEN_OF_7] + c[LEN_OF_8])
 
-    tot = 0
-    for result in results:
-        value = ''.join(str(list_of_bits.index(set(v))) for v in result)
-        print(value)
+    total = 0
+    for line in lines:
+        digits = calculateDigit([v.strip() for v in line.replace(' | ', ' ').split(' ')])
+        for i in range(len(digits)):
+            assert digits[i] not in digits[i+1:], f"{i} {digits[i:].index(digits[i])}"
+
+        results = line.split(' | ')[1].split(' ')
+        value = ''.join(str(digits.index(set(result))) for result in results)
+        total += int(value)
+
+    print(f"Part 2: {total}")
 
 
 if __name__ == "__main__":
